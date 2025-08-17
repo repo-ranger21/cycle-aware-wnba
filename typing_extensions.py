b72c5a03cb41ec8b3f6dd5dd9e53e2664b92c8bb
repo-ranger21 <chris.peterson6995@gmail.com>
2025-08-17 15:42:49 -0400

    Create typing_extensions.py with default contents

---
+++ typing_extensions.py
@@ -0,0 +1,20 @@
+# typing_extensions.py
+
+# This file is a placeholder for the typing_extensions module.
+# The actual content can vary depending on the version of Python and the typing_extensions library.
+
+def overload(func):
+    """Decorator to indicate that a function can be overloaded."""
+    return func
+
+def final(cls):
+    """Decorator to indicate that a class cannot be subclassed."""
+    return cls
+
+def Literal(*args):
+    """A type that can be any one of the specified literal values."""
+    return args
+
+def TypedDict(*args, **kwargs):
+    """A type that represents a dictionary with a fixed set of keys."""
+    return dict
